fbb09c540c2ca4c7204e8ac4ef2569c9276a0a50
rjlovesu <rjlovesu@github.com>
2020-05-18 20:05:53 +0300

    hopefully fixed scenario 1

--- src/main/java/pages/pageLib/ChangePasswordPage.java
+++ src/main/java/pages/pageLib/ChangePasswordPage.java
@@ -7,6 +7,10 @@ public class ChangePasswordPage extends BasePage {
   public ChangePasswordPage() {
     addElement("title", "//h1[contains(., 'Change Password')]");
     addElement("currentPasswordField", "input#currentPassword");
+    addElement("newPasswordField", "input#newPassword");
+    addElement("repeatNewPasswordField", "input#newPasswordRepeat");
+    addElement("change", "button#changeButton");
+  
   }
   @Override
   public SelenideElement getMainElement() {
